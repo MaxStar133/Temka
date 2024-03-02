c1045db8e22d077b61f0196ecdb259e7207162ca
MaxStar133 <ov4innickovmax@yandex.ru>
2024-03-03 01:45:39 +0300

    Patch 1
    Добавлено:
    * Спрайты главного персонажа а также анимации спокойствия и ходьбы.
    Изменено:
    * Теперь при вращении мышки крутится не только ган но и весь player

--- Assets/Scripts/Weapon/Pistolet/Pistolet.cs
+++ Assets/Scripts/Weapon/Pistolet/Pistolet.cs
@@ -26,9 +26,9 @@ public class Pistolet : MonoBehaviour
     private void Update()
     {
         // Как я понял позволяет вращать оружием
-        Vector3 diference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        /*Vector3 diference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         float rotateZ = Mathf.Atan2(diference.y, diference.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0f, 0f, rotateZ);
+        transform.rotation = Quaternion.Euler(0f, 0f, rotateZ); */
         //
         // Проверка на стрельбу
         if (Input.GetMouseButton(0) && canShoot && currentAmmo > 0)
--- Assets/Scripts/player/PlayerMove.cs
+++ Assets/Scripts/player/PlayerMove.cs
@@ -3,8 +3,9 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
    [SerializeField] private float speed;
+    [SerializeField] private Animator anim;
 
-   private Rigidbody2D rg;
+    private Rigidbody2D rg;
    private Vector2 moveInput;
 
    private void Start()
@@ -14,13 +15,32 @@ public class PlayerMove : MonoBehaviour
 
    private void Update()
    {
-    moveInput.x = Input.GetAxisRaw("Horizontal");
-    moveInput.y = Input.GetAxisRaw("Vertical");
+        Vector3 diference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float rotateZ = Mathf.Atan2(diference.y, diference.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotateZ);
+
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+         moveInput.y = Input.GetAxisRaw("Vertical");
+
+        CheckAnimation();
+
 
    }
     private void FixedUpdate()
     {
         if (rg.bodyType != RigidbodyType2D.Static)
         rg.MovePosition(rg.position + moveInput * speed * Time.fixedDeltaTime);
+    }
+
+    private void CheckAnimation()
+    {
+        if (moveInput.magnitude > 0)
+        {
+            anim.SetInteger("state", 1);
+        }
+        else
+        {
+            anim.SetInteger("state", 0);
+        }
     }
 }
